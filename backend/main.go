df95e1f1365226b17a0e8b60297082e807f6432a
e.an <161005688+estateangela@users.noreply.github.com>
2025-09-21 10:29:55 +0800

    ｆｉｘ
    Co-Authored-By: Dennislee:) <55692615+dennislee928@users.noreply.github.com>

--- backend/main.go
+++ backend/main.go
@@ -27,7 +27,7 @@ import (
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host localhost:8080
+// @host mindhelp.onrender.com
 // @BasePath /api/v1
 
 // @securityDefinitions.apikey BearerAuth
