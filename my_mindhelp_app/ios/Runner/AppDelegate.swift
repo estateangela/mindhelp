a3c5473498271096915a46d3f53fb2538b35d070
e.an <161005688+estateangela@users.noreply.github.com>
2025-10-02 13:33:21 +0800

    fix ios app

--- my_mindhelp_app/ios/Runner/AppDelegate.swift
+++ my_mindhelp_app/ios/Runner/AppDelegate.swift
@@ -8,11 +8,12 @@ import GoogleMaps
       _ application: UIApplication,
       didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // 這裡從環境變數讀取 Google Maps API Key
-    if let mapsApiKey = ProcessInfo.processInfo.environment["GOOGLE_MAPS_API_KEY"] {
+    // 這裡從 Info.plist 讀取 GOOGLE_MAPS_API_KEY
+    if let mapsApiKey = Bundle.main.infoDictionary?["GOOGLE_MAPS_API_KEY"] as? String {
         GMSServices.provideAPIKey(mapsApiKey)
     } else {
-        fatalError("GOOGLE_MAPS_API_KEY is not set in environment variables.")
+        // 如果金鑰未設定，拋出致命錯誤，提醒開發者檢查配置
+        fatalError("GOOGLE_MAPS_API_KEY is not set in Info.plist. Please check your .xcconfig file.")
     }
 
     GeneratedPluginRegistrant.register(with: self)
